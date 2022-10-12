e3f7768a089d0ac66047ac68a0b9145a287ed7ae
KevinRAV <tsiorisoa-kevin.raveloarimanana@edu.esiee-it.fr>
2022-10-12 16:18:00 +0200

    add template database

---
+++ sql_app/models.py
@@ -0,0 +1,85 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Float, DateTime
+from sqlalchemy.orm import relationship, foreign
+
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_admin = Column(Boolean, default=True)
+
+    products = relationship("Product", back_populates="seller")
+    carts = relationship("Cart", back_populates="owner")
+    comments = relationship("Comment", back_populates="author")
+    commands = relationship("Command", back_populates="buyer")
+    commands = relationship("Command", back_populates="seller")
+
+
+class Product(Base):
+    _tablename__ = "products"
+
+    jd = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    price = Column(Float, index=True)
+    description = Column(String)
+    stock = Column(Integer, index=True)
+    image = Column(String, index=True)
+    category_id = Column(Integer, ForeignKey("categories.id"))
+    seller_id = Column(Integer, ForeignKey("users.id"))
+
+    seller = relationship("User", back_populates="products")
+    category = relationship("Category", back_populates="products")
+    products = relationship("Cart", back_populates="product")
+    comments = relationship("Comment", back_populates="product")
+
+class Category(Base):
+    _tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+
+    products = relationship("Product", back_populates="category")
+
+class Cart(Base):
+    _tablename__ = "carts"
+
+    owner_id = Column(Integer, ForeignKey("users.id"), primary_key=True, index=True)
+    product_id = Column(Integer, ForeignKey("products.id"), primary_key=True, index=True)
+    quantity = Column(Integer)
+
+    owner = relationship("User", back_populates="carts")
+    product = relationship("Product", back_populates="carts")
+
+class Comment(Base):
+    _tablename__ = "comments"
+
+    author_id = Column(Integer, ForeignKey("users.id"), primary_key=True, index=True)
+    product_id =  Column(Integer, ForeignKey("products.id"), primary_key=True, index=True)
+    stars = Column(Integer)
+    message = Column(String)
+
+    author = relationship("User", back_populates="comments")
+    product = relationship("Product", back_populates="comments")
+
+class Commands(Base):
+    _tablename__ = "commands"
+
+    id = Column(Integer, primary_key=True, index=True)
+    product_name = Column(String, index=True)
+    quantity = Column(Integer, index=True)
+    timestamp = Column(DateTime, default=datetime.datetime.utcnow)
+    unit_price = Column(Float, index=True)
+
+    buyer = relationship("User", back_populates="commands")
+    seller = relationship("User", back_populates="commands")
+
+
+
+
+
+
