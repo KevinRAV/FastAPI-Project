9c2f804460196121765c984fe4d97d45601747f7
KevinRAV <tsiorisoa-kevin.raveloarimanana@edu.esiee-it.fr>
2022-10-13 13:41:08 +0200

    Fix request create_user in sql_app/main.py

--- sql_app/main.py
+++ sql_app/main.py
@@ -1,8 +1,10 @@
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
+from models.user import User, UserCreate
 from . import crud
 from . import models
+
 
 from .database import SessionLocal, engine
 
@@ -20,9 +22,9 @@ def get_db():
         db.close()
 
 
-@app.post("/users/", response_model=models.user)
-def create_user(user: models.user.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
+@app.post("/users/", response_model=User)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, name=user.name, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db=db, user=user)
