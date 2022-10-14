ae66b5bb395065b5231e53c3c3a198d5d640aa82
Julien ROBIN <julien.jean.robin@gmail.com>
2022-10-14 14:30:58 +0200

    get et post /categories

--- routers/categories.py
+++ routers/categories.py
@@ -1,23 +1,33 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Header
 
 from models.category import *
 from sql_app import crud, models
 from sqlalchemy.orm import Session
 from sql_app.database import get_db
+from utils.auth import read_token
 
 router = APIRouter()
 
 
 @router.get("/categories")
-def get_categories(search: str = "", sort: str = ""):
-    categories = {"searching": search, "sorting": sort}
-    return categories
+def get_categories(name: str = "", id: int | None = None, db: Session = Depends(get_db)):
+    query = []
+    if len(name) > 0:
+        query = query.append(["name", name])
+    if id is not None:
+        query = query.append(["id", id])
+    return crud.read(models.Category, query, db)
 
 
 @router.post("/categories", response_model=Category)
-def create_categories(category: CategoryCreate, db: Session = Depends(get_db)):
-    db_category = crud.get_category_name(db, name=category.name)
+def create_categories(category: CategoryCreate,
+                      auth_token: str = Header(min_length=1, max_length=50),
+                      db: Session = Depends(get_db)
+                      ):
+    if not read_token(auth_token, db).is_admin:
+        raise HTTPException(status_code=401, detail="Only admins can manage categories")
+    db_category = crud.read(models.Category, [["name", category.name]], db)
     if not db_category:
-        row = models.User(**category.dict())
+        row = models.Category(**category.dict())
         return crud.create(row, db)
     raise HTTPException(status_code=400, detail="Category already registered")
