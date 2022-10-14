c37f8b4d0c8e50d1c2097035c89ae88264d9949a
KevinRAV <tsiorisoa-kevin.raveloarimanana@edu.esiee-it.fr>
2022-10-14 13:24:38 +0200

    Request API carts functionnal (only delete in progress)

--- sql_app/main.py
+++ sql_app/main.py
@@ -1,12 +1,11 @@
-from fastapi import Depends, FastAPI, HTTPException, Query
+from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
-from typing import Union
 
-from models import category, user
-from models.cart import Cart
-from models.user import User, UserCreate
+from models.cart import Cart, CartCreate
 from models.category import Category, CategoryCreate
+from models.command import Command, CommandCreate
 from models.product import Product, ProductCreate
+from models.user import User, UserCreate
 from . import crud
 from . import models
 from .database import SessionLocal, engine
@@ -97,3 +96,31 @@ def delete_product(id: int, db: Session = Depends(get_db)):
 @app.put("/products/{id}")
 def update_product(id: int, product: Product, db: Session = Depends(get_db)):
     return crud.update_product(db, id, product)
+
+
+@app.post("/carts/", response_model=Cart)
+def create_user_cart(
+        owner_id: int, product_id: int, cart: CartCreate, db: Session = Depends(get_db)):
+    return crud.create_cart(db=db, cart=cart, owner_id=owner_id, product_id=product_id)
+
+
+@app.get("/carts/", response_model=list[Cart])
+def read_cart(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    carts = crud.get_carts(db, skip=skip, limit=limit)
+    return carts
+
+
+@app.delete("/carts/{product_id}")
+def delete_cart_by_id(product_id: int, db: Session = Depends(get_db)):
+    return crud.delete_cart_by_id(db, product_id)
+
+
+@app.patch("/carts/{product_id}")
+def update_cart(product_id: int, cart: Cart, db: Session = Depends(get_db)):
+    return crud.update_cart(db, product_id, cart)
+
+
+@app.post("/commands/", response_model=Command)
+def create_user_command(
+        buyer_id: int, command: CommandCreate, db: Session = Depends(get_db)):
+    return crud.create_command(db=db, command=command, buyer_id=buyer_id)
