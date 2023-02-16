90c7b4358b165383d37279c51c0f77930a4a0c84
Jerance <michael.jerance@gmail.com>
2023-02-16 08:09:12 +0100

    Fix CORS for fastAPI project

--- main.py
+++ main.py
@@ -17,7 +17,24 @@ from models.product import Product, ProductCreate
 from models.user import User, UserCreate
 from sql_app.database import get_db
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.delete("/categories/{id}", response_model=Category)
